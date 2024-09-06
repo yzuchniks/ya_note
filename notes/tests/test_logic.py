971b55eb7536a336d07b78ee02752752b64559eb
Sergey Morgunov <Bo-Jack-Bo@yandex.ru>
2024-09-06 19:18:32 +0400

    Дописал тесты удаления и редактирования заметок.

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from pytils.translit import slugify
 
 from django.contrib.auth import get_user_model
@@ -52,3 +53,71 @@ class TestNoteCreate(TestCase):
         response = self.auth_client.post(self.CREATE_URL, self.form_data)
         self.assertFormError(response, 'form', 'slug', slug + WARNING)
         self.assertEqual(Note.objects.count(), 1)
+
+
+class TestNoteEditDelete(TestCase):
+    """Тесты редактирования и удаления заметки."""
+    NOTE_TEXT = 'Текст заметки'
+    NOTE_TITLE = 'Заголовок заметки'
+    NEW_NOTE_TEXT = 'Новый текст заметки'
+    NEW_NOTE_TITLE = 'Новый заголовок заметки'
+    DONE_URL = reverse('notes:success')
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.good_user = User.objects.create(username='GoodUser')
+        cls.bad_user = User.objects.create(username='BadUser')
+        cls.note = Note.objects.create(
+            title=cls.NOTE_TITLE,
+            text=cls.NOTE_TEXT,
+            author=cls.good_user,
+        )
+        arg_slug = (cls.note.slug,)
+        cls.edit_url = reverse('notes:edit', args=arg_slug)
+        cls.delete_url = reverse('notes:delete', args=arg_slug)
+        cls.good_client = Client()
+        cls.good_client.force_login(cls.good_user)
+        cls.bad_client = Client()
+        cls.bad_client.force_login(cls.bad_user)
+        cls.form_data = {
+            'title': cls.NEW_NOTE_TITLE, 'text': cls.NEW_NOTE_TEXT
+        }
+
+    def test_author_can_delete_note(self):
+        """Автор заметки может удалить заметку."""
+        response = self.good_client.post(self.delete_url)
+        self.assertRedirects(response, self.DONE_URL)
+        self.assertEqual(Note.objects.count(), 0)
+
+    def test_other_user_cant_delete_note(self):
+        """Другой пользователь не может удалить заметку."""
+        self.bad_client.post(self.delete_url)
+        self.assertEqual(Note.objects.count(), 1)
+
+    def test_anonymous_cant_delete_note(self):
+        """Анонимный пользователь не может удалить заметку."""
+        self.client.post(self.delete_url)
+        self.assertEqual(Note.objects.count(), 1)
+
+    def test_author_can_edit_note(self):
+        """Автор заметки может редактировать заметку."""
+        response = self.good_client.post(self.edit_url, self.form_data)
+        self.assertRedirects(response, self.DONE_URL)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.title, self.NEW_NOTE_TITLE)
+        self.assertEqual(self.note.text, self.NEW_NOTE_TEXT)
+
+    def test_other_user_cant_edit_note(self):
+        """Другой пользователь не может редактировать заметку."""
+        response = self.bad_client.post(self.edit_url, self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.title, self.NOTE_TITLE)
+        self.assertEqual(self.note.text, self.NOTE_TEXT)
+
+    def test_anonymous_cant_edit_note(self):
+        """Анонимный пользователь не может редактировать заметку."""
+        self.client.post(self.edit_url, self.form_data)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.title, self.NOTE_TITLE)
+        self.assertEqual(self.note.text, self.NOTE_TEXT)
